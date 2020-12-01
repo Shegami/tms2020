2a444d7fc2d34914046f58c86f780bab02d56543
Kanstantsin Toukach <kanstantsin.toukach@gmail.com>
2020-12-01 21:54:36 +0300

    TMS

---
+++ strings/strings_01.py
@@ -0,0 +1,10 @@
+"""
+Создать переменные firstname, lastname, age с соотв. значениями.
+Путём сложения получить строку типа: "Привет, меня зовут firstname last name, мне age лет.
+"""
+
+firstname = 'Костя'
+lastname = 'Товкач'
+age = '21'
+
+print('Привет, меня зовут ' + firstname + ' ' + lastname + ', мне ' + age + ' год')
