a544d3dffee64064eeb897297c27be9a906ba1a6
Kanstantsin Toukach <kanstantsin.toukach@gmail.com>
2020-12-10 20:22:34 +0300

    class tasks

---
+++ loops/loops_01.py
@@ -0,0 +1,10 @@
+"""
+Вывести на экран фразу 'Silence is golden' n раз.
+n вводит пользователь.
+"""
+
+n = int(input('Enter a number: '))
+
+while n != 0:
+    print('Silence is golden')
+    n -= 1
