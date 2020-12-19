dd3aee00c28b4fd1f2de1be8f9787f5289845d33
Shegami <kanstantsin.toukach@gmail.com>
2020-12-19 21:58:57 +0300

    struct_prog_08

--- struct_prog/struct_prog_13.py
+++ struct_prog/struct_prog_13.py
@@ -1,12 +1,35 @@
 """
-
+Дан словарь:
+{'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
+Добавить каждому ключу число равное длине этого ключа
+(пример {‘key’: ‘value’} -> {‘key3’: ‘value’}).
+Чтобы получить список ключей - использовать метод .keys()
 """
 
-def name(var):
-    pass
-    
+
+def key_len_for(dictt):
+    for key in list(dictt.keys()):
+        dictt[key + str(len(key))] = dictt.pop(key)
+    print(dictt)
+
+
+def key_len_while(dictt):
+    keys = list(dictt.keys())
+    index = 0
+    while True:
+        if index != len(keys):
+            dictt[keys[index] + str(len(keys[index]))] = dictt.pop(keys[index])
+            index += 1
+        else:
+            break
+    print(dictt)
+
+
 def main():
-    pass
-    
+    dictt = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
+    key_len_for(dictt)
+#    key_len_while(dictt)
+
+
 if __name__ == '__main__':
     main()
--- struct_prog/struct_prog_17.py
+++ struct_prog/struct_prog_17.py
@@ -1,12 +1,20 @@
 """
-
+Дано число. Найти сумму и произведение его цифр.
 """
 
-def name(var):
-    pass
-    
+
+def sum_mult(number):
+    summ = mult = number[0]
+    for i in number[1::]:
+        summ += i
+        mult *= i
+    print(summ, mult)
+
+
 def main():
-    pass
-    
+    number = int(input('Your number: '))
+    sum_mult(number)
+
+
 if __name__ == '__main__':
     main()
