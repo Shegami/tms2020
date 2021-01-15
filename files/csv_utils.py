2e098c993bde2a73c547785c1baec3d9484ad4e7
Shegami <71257025+Shegami@users.noreply.github.com>
2021-01-15 16:35:30 +0300

    Update csv_utils.py

--- files/csv_utils.py
+++ files/csv_utils.py
@@ -19,21 +19,21 @@ def csv_write(filename, fields, data):
         csvwriter.writerows(data)
 
 
-def csv_add_row(filename, row, position=None):
+def csv_add_row(filename, row, position=-1):
     fields, data = csv_read(filename)
-    if position is None:
+    if position == -1:
         data.append(row)
     else:
-        data.insert(position-1, row)
+        data.insert(position - 1, row)
     csv_write(filename, fields, data)
 
 
-def csv_pop_row(filename, position=None):
+def csv_pop_row(filename, position=-1):
     fields, data = csv_read(filename)
-    if position is None:
+    if position == -1:
         data.pop()
     else:
-        data.pop(position-1)
+        data.pop(position - 1)
     csv_write(filename, fields, data)
 
 
@@ -66,10 +66,25 @@ def csv_goods_cheapest(filename):
 def csv_goods_reduce(filename, quantity=1):
     fields, data = csv_read(filename)
     for row in range(len(data)):
-        print(f'{row+1} {data[row][0]}')
+        print(f'{row + 1} {data[row][0]}')
     choice = int(input('Выберите номер товара: ')) - 1
     if quantity > int(data[choice][2]):
         print('Ошибка. Указанное кол-во больше, чем кол-во товара.')
     else:
         data[choice][2] = str(int(data[choice][2]) - quantity)
     csv_write(filename, fields, data)
+
+
+def csv_age_sort(data_input, data_output):
+    for i in range(len(data_input)):
+        if data_input[i][2] < 13:
+            data_output[0][0] += 1
+        elif 12 < data_input[i][2] < 19:
+            data_output[0][1] += 1
+        elif 18 < data_input[i][2] < 26:
+            data_output[0][2] += 1
+        elif 25 < data_input[i][2] < 41:
+            data_output[0][3] += 1
+        else:
+            data_output[0][4] += 1
+    return data_output
