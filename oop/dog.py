612a937a2e62bc1b30150184a2f383eeafe06f6c
Kanstantsin Toukach <kanstantsin.toukach@gmail.com>
2021-01-25 17:36:00 +0300

    'oop_04'

--- oop/dog.py
+++ oop/dog.py
@@ -1,4 +1,12 @@
 class Dog:
+    # oop_2
+    def bark(self):
+        print('Woof Woof!')
+
+    def look_at_moon(self):
+        for i in range(3):
+            self.bark()
+
     # oop_03
     def jump(self):
         print('Jump!')
@@ -6,12 +14,24 @@ class Dog:
     def run(self):
         print('Run!')
 
+    # oop_04
+    def __init__(self, height,
+                 weight, name, age):
+        self.height = height
+        self.weight = weight
+        self.name = name
+        self.age = age
+
 
 def main():
-    dog = Dog()
-    print(dog)
+    dog = Dog(1.5, 13, 'Stefan', 7)
     dog.jump()
     dog.run()
+    dog.bark()
+    print(dog.height)
+    print(dog.weight)
+    print(dog.name)
+    print(dog.age)
 
 
 if __name__ == '__main__':
