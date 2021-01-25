6572e324cf8214efc6bed40a05778b28b8c25641
Shegami <71257025+Shegami@users.noreply.github.com>
2021-01-25 22:29:58 +0300

    oop_08

--- oop/dog.py
+++ oop/dog.py
@@ -18,38 +18,77 @@ class Dog:
     def __init__(self, height,
                  weight, name,
                  age, master):
-        self.height = height
-        self.weight = weight
-        self.name = name
-        self.age = age
+        self.__height = height
+        self.__weight = weight
+        self.__name = name
+        self.__age = age
         self.__master = master
         self.__address = 'Minsk'
 
-    # oop_05
-    def change_name(self, name):
-        self.name = name
+    # oop_08
+    @property
+    def height(self):
+        return self.__height
 
-    # oop_06
-    def get_master(self):
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, weight):
+        self.__weight = weight
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        self.__age = age
+
+    @property
+    def master(self):
         return self.__master
 
-    # oop_07
-    def get_address(self):
+    @master.setter
+    def master(self, master):
+        self.__master = master
+
+    @property
+    def address(self):
         return self.__address
 
-    def set_address(self, address):
+    @address.setter
+    def address(self, address):
         self.__address = address
 
 
 def main():
     dog = Dog(1.5, 13, 'Stefan', 7, 'KOSTIA')
+    dog.name = 'Izmail'
     print(dog.name)
-    dog.change_name('Mustang')
-    print(dog.name)
-    print(dog.get_master())
-    print(dog.get_address())
-    dog.set_address('Piter')
-    print(dog.get_address())
+    dog.weight = 1
+    print(dog.weight)
+    dog.height = 12
+    print(dog.height)
+    dog.age = 76
+    print(dog.age)
+    dog.master = 'ALESA'
+    print(dog.master)
+    dog.address = 'Mahachkala'
+    print(dog.address)
 
 
 if __name__ == '__main__':
