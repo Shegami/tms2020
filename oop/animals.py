dd0de87b4e019181484301bbe4c1c67c0b3ceb70
Kanstantsin Toukach <kanstantsin.toukach@gmail.com>
2021-01-26 20:18:35 +0300

    oop_12

--- oop/animals.py
+++ oop/animals.py
@@ -1,14 +1,14 @@
 """
-Создать файл animals.py. Создать три класса: Dog, Cat, Parrot.
-Атрибуты каждого класса: name, age, master.
-Каждый класс содержит конструктор и методы:
-run, jump, birthday(увеличивает age на 1), sleep.
-Класс Parrot имеет дополнительный метод fly.
-Cat - meow, Dog - bark.
+Создать родительский класс Pet, содержащий
+все общие методы классов
+og, Cat, Parrot. Унаследовать Dog, Cat, Parrot от класса Pet.
+Удалить в дочерних классах те методы, которые имеются у
+родительского класса.
+Создать объект каждого класса и вызвать все его методы.
 """
 
 
-class Dog:
+class Pet:
     def __init__(self,
                  name, age, master):
         self.name = name
@@ -16,53 +16,28 @@ class Dog:
         self.master = master
 
     def run(self):
-        return 'Run, dog!'
+        return 'Run, bitch!'
 
     def jump(self):
-        return 'Jump, dog!'
+        return 'Jump, bitch!'
 
     def bithday(self):
         self.age += 1
+
+
+class Dog(Pet):
 
     def bark(self):
         return 'Woof!'
 
 
-class Cat:
-    def __init__(self,
-                 name, age, master):
-        self.name = name
-        self.age = age
-        self.master = master
-
-    def run(self):
-        return 'Run, cat!'
-
-    def jump(self):
-        return 'Jump, cat!'
-
-    def birthday(self):
-        self.age += 1
+class Cat(Pet):
 
     def meow(self):
         return 'Meow!'
 
 
-class Parrot:
-    def __init__(self,
-                 name, age, master):
-        self.name = name
-        self.age = age
-        self.master = master
-
-    def run(self):
-        return 'Run, parrot!'
-
-    def jump(self):
-        return 'Jump, parrot!'
-
-    def birthday(self):
-        self.age += 1
+class Parrot(Pet):
 
     def fly(self):
         return 'Fly!'
