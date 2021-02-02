a3f102848497a6708134d9c033a19f726f05a295
Kanstantsin Toukach <kanstantsin.toukach@gmail.com>
2021-02-02 21:47:54 +0300

    create folder

---
+++ scripts/scripts_03.py
@@ -0,0 +1,22 @@
+"""
+Создать скрипт, который принимает имя папки
+и создает ее рядом со скриптом
+"""
+
+import os
+import sys
+
+
+file_path = os.path.realpath(__file__)
+print(file_path)
+dir_name = os.path.dirname(file_path)
+print(dir_name)
+os.mkdir(f'{dir_name}/{sys.argv[1]}')
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
