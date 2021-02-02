0359db5e8b88a027c37f3d8aa688e837532db115
Kanstantsin Toukach <kanstantsin.toukach@gmail.com>
2021-02-02 21:18:14 +0300

    scripts_02

---
+++ scripts/scripts_02.py
@@ -0,0 +1,42 @@
+"""
+Создать скрипт, который принимает имя, фамилию и возраст
+и дописывает их в файл
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-fn',
+    '--first-name',
+    required=True
+)
+parser.add_argument(
+    '-ln',
+    '--last-name',
+    required=True
+)
+parser.add_argument(
+    '-ag',
+    '--age',
+    required=True
+)
+
+args = parser.parse_args()
+print('First name:', args.first_name)
+print('Last name:', args.last_name)
+print('Age:', args.age)
+
+
+with open('scripts/scripts_02.txt', 'w') as my_file:
+    my_file.write(f"First name: {args.first_name}\n"
+                  f"Last name: {args.last_name}\n"
+                  f"Age: {args.age}\n")
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
