47b83419301c70d57bce4c3821a4502daaf025af
Kanstantsin Toukach <kanstantsin.toukach@gmail.com>
2021-02-11 19:57:05 +0300

    sqlalchemy_01

---
+++ sqlalchemy/sqlalchemy_01.py
@@ -0,0 +1,30 @@
+"""
+Все операции выполнять с помощью sqlalchemy.
+Создать файл sqlalchemy_01.py. Создать базу sa_test.db.
+Создать таблицу Book с помощью sqlalchemy.
+Атрибуты: id(integer primary key),
+title(varchar), pages(int), author(varchar),
+price(float), release_year(int)
+"""
+
+
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///sa_test.db')
+engine.execute("""
+    create table Book (
+    id int prinary key,
+    title varchar,
+    pages int,
+    author varchar,
+    price float,
+    release_year int);
+""")
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
