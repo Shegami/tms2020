30e5d579b89d2ec7f67a05f1f30e83b468fad8b6
Shegami <kanstantsin.toukach@gmil.com>
2021-02-18 20:29:45 +0300

    sqlaclhemy_12

--- my_sqlalchemy/school/sqlalchemy_12.py
+++ my_sqlalchemy/school/sqlalchemy_12.py
@@ -7,28 +7,12 @@
 Получить всех студентов и создать для каждого дневник.
 """
 
-from my_sqlalchemy.school.models import session,\
+from my_sqlalchemy.school.models import session, \
     Student, Diary
 
-students = session.query(Student.lastname)
+students = session.query(Student)
 for student in students:
     print(student)
-
-diary_1 = Diary(avg_mark=9, student_id=1)
-diary_2 = Diary(avg_mark=4, student_id=2)
-diary_3 = Diary(avg_mark=3, student_id=3)
-diary_4 = Diary(avg_mark=6, student_id=4)
-diary_5 = Diary(avg_mark=8, student_id=5)
-diary_6 = Diary(avg_mark=10, student_id=6)
-
-session.add_all(
-    [
-        diary_6,
-        diary_5,
-        diary_4,
-        diary_3,
-        diary_2,
-        diary_1
-    ]
-)
+    diary = Diary(avg_mark=9, student=student)
+    session.add(diary)
 session.commit()
