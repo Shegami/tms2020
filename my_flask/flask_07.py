6e96c9365ee6f541f6e9eb38b33ef0e6ca6abec2
Kanstantsin Toukach <kanstantsin.toukach@gmail.com>
2021-03-04 11:54:08 +0300

    'flask_hw_25'

--- my_flask/flask_07.py
+++ my_flask/flask_07.py
@@ -8,18 +8,19 @@
 на страницу отображения всех групп.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='../templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///groups.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///groups.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Group(db.Model):
     id = db.Column(
-        db.Integer,
-        primary_key=True
+         db.Integer,
+         primary_key=True
     )
     name = db.Column(
         'name',
@@ -31,10 +32,21 @@ db.init_app(app)
 db.create_all()
 
 
-@app.route('/groups', methods=['GET'])
-def groups():
-    groupss = Group.query.all()
-    return render_template('groups.html', groups=groupss)
+@app.route('/get_group')
+def get_groups():
+    groups = Group.query.all()
+    return render_template('get_group.html', groups=groups)
+
+
+@app.route('/create_group', methods=['POST', 'GET'])
+def create_group():
+    if request.method == 'GET':
+        return render_template('create_group.html')
+    else:
+        new_group = Group(name=request.form['group_name'])
+        db.session.add(new_group)
+        db.session.commit()
+        return redirect(url_for('get_groups'))
 
 
 if __name__ == '__main__':
