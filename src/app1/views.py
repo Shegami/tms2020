c4d605349412e48faacf84f889c559a1e99ff896
Shegami <kanstantsin.toukach@gmail.com>
2021-03-06 16:27:49 +0300

    'django_03'

--- src/app1/views.py
+++ src/app1/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from datetime import date
 
 
@@ -9,3 +10,10 @@ def date_now(request):
 def two_pow(request, number):
     assert isinstance(number, int)
     return HttpResponse(f'{2 ** number}')
+
+
+def my_word(request, word):
+    if len(word) % 2 == 0:
+        return HttpResponse(f'{word[1::2]}')
+    else:
+        redirect('main')
