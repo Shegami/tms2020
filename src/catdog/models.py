c43bbadaf84d2c3720872f078d36745f04db6269
Kanstantsin Toukach <kanstantsn.toukach@gmail.com>
2021-03-13 14:29:08 +0300

    'django_21'

--- src/catdog/models.py
+++ src/catdog/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class AnimalImage(models.Model):
     url = models.URLField()
-    spicies = models.CharField(max_length=3)
+    species = models.CharField(max_length=3)
     date = models.DateTimeField()
     type = models.CharField(max_length=4)
+
+    def __str__(self):
+        return f'{self.id}. {self.species}'
--- src/catdog/views.py
+++ src/catdog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 import requests
+from catdog.models import AnimalImage
+from datetime import datetime
 
 
 def catdog(request):
@@ -13,22 +15,38 @@ def catdog(request):
 
 
 def cat(request):
+    data = requests.get('https://aws.random.cat/meow').json()
+    context = {
+        'url': data['file']
+    }
     if request.method == 'GET':
-        data = requests.get('https://aws.random.cat/meow').json()
-        context = {
-            'link': data['file']
-        }
         return render(request, 'cat.html', context)
     else:
+        if request.POST.get('button') == 'save':
+            typpe = context['url'].split('.')[-1]
+            save_model(context['url'], typpe).save()
         return redirect(catdog)
 
 
 def dog(request):
+    data = requests.get('https://dog.ceo/api/breeds/image/random').json()
+    context = {
+        'url': data['message']
+    }
     if request.method == 'GET':
-        data = requests.get('https://dog.ceo/api/breeds/image/random').json()
-        context = {
-            'link': data['message']
-        }
         return render(request, 'dog.html', context)
     else:
+        if request.POST.get('button') == 'save':
+            typpe = context['url'].split('.')[-1]
+            save_model(context['url'], typpe).save()
         return redirect(catdog)
+
+
+def save_model(url, typpe):
+    model = AnimalImage(
+                url=url,
+                species='dog',
+                date=datetime.now(),
+                type=typpe
+            )
+    return model
