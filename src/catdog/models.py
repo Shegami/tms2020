a6432a0cade0548d816e7de3c180e7ab9ca95617
Kanstantsin Toukach <kanstantsn.toukach@gmail.com>
2021-03-13 12:45:21 +0300

    'django_18_19_20'

--- src/catdog/models.py
+++ src/catdog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class AnimalImage(models.Model):
+    url = models.URLField()
+    spicies = models.CharField(max_length=3)
+    date = models.DateTimeField()
+    type = models.CharField(max_length=4)
--- src/catdog/views.py
+++ src/catdog/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render, redirect
+import requests
 
 
 def catdog(request):
     if request.method == 'GET':
-        return render(request, 'catdog')
+        return render(request, 'catdog.html')
+    else:
+        if 'cat' in request.POST:
+            return redirect('cat')
+        else:
+            return redirect('dog')
+
+
+def cat(request):
+    if request.method == 'GET':
+        data = requests.get('https://aws.random.cat/meow').json()
+        context = {
+            'link': data['file']
+        }
+        return render(request, 'cat.html', context)
+    else:
+        return redirect(catdog)
+
+
+def dog(request):
+    if request.method == 'GET':
+        data = requests.get('https://dog.ceo/api/breeds/image/random').json()
+        context = {
+            'link': data['message']
+        }
+        return render(request, 'dog.html', context)
+    else:
+        return redirect(catdog)
