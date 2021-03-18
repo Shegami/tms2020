18a35a3697bf2fec8f9fd04a425975c5256e37d8
Kanstantsin Toukach <kanstantsn.toukach@gmail.com>
2021-03-18 17:54:06 +0300

    'drf_06'

--- src/drf1/views.py
+++ src/drf1/views.py
@@ -26,9 +26,26 @@ def api_products(request):
             )
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def api_products_detailed(request, pk):
+    product = Product.objects.get(pk=pk)
     if request.method == 'GET':
-        products = Product.objects.get(pk=pk)
-        serializer = ProductSerializer(products)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+            )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    elif request.method == 'DELETE':
+        product.delete()
+    return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
