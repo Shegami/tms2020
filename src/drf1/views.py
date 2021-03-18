da3f2a5c6c6c4a8d31341391559b8da020a8ff77
Shegami <kanstantsin.toukach@gmail.com>
2021-03-18 20:13:19 +0300

    -status

--- src/drf1/views.py
+++ src/drf1/views.py
@@ -1,7 +1,5 @@
 from drf1.models import Product
 from drf1.serializers import ProductSerializer
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, \
     RetrieveUpdateDestroyAPIView
 
