c2a2b0d168e54251251ee94199adaa1cc56a2839
Shegami <kanstantsin.toukach@gmail.com>
2021-03-18 20:12:45 +0300

    drf_08

--- src/drf1/views.py
+++ src/drf1/views.py
@@ -2,52 +2,15 @@ from drf1.models import Product
 from drf1.serializers import ProductSerializer
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, \
+    RetrieveUpdateDestroyAPIView
 
 
-class APIProducts(APIView):
-    def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors,
-            status.HTTP_400_BAD_REQUEST
-        )
+class APIProducts(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
-class APIProductDetailed(APIView):
-    def get(self, request, pk):
-        product = Product.objects.get(pk=pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        product = Product.objects.get(pk=pk)
-        serializer = ProductSerializer(product, request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data,
-            )
-        else:
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-    def delete(self, request, pk):
-        product = Product.objects.get(pk=pk)
-        product.delete()
-        return Response(
-                status=status.HTTP_204_NO_CONTENT
-            )
+class APIProductDetailed(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
